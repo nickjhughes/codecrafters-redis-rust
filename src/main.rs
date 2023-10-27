c304b640dda17d6c195c6ea8a37a737daa3b80f8
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2023-10-27 20:11:59 +1100

    Handle SET and GET

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::{Ipv4Addr, SocketAddrV4};
+use std::{
+    collections::HashMap,
+    net::{Ipv4Addr, SocketAddrV4},
+    sync::{Arc, Mutex},
+};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
@@ -13,8 +17,11 @@ const PORT: u16 = 6379;
 
 #[derive(Debug)]
 enum Request<'data> {
+    CommandDocs,
     Ping,
     Echo(&'data str),
+    Set { key: &'data str, value: &'data str },
+    Get(&'data str),
 }
 
 impl<'data> Request<'data> {
@@ -24,59 +31,117 @@ impl<'data> Request<'data> {
         }
         let (request_value, _) = RespValue::deserialize(data)?;
         match request_value {
-            RespValue::Array(elements) => {
-                if elements.is_empty() {
-                    return Err(anyhow::format_err!("empty array request"));
-                }
-                match elements[0] {
-                    RespValue::BulkString(s) => match s.to_lowercase().as_str() {
-                        "ping" => {
-                            return Ok(Request::Ping);
-                        }
-                        "echo" => {
-                            if elements.len() == 2 {
-                                match elements[1] {
-                                    RespValue::BulkString(s) => {
-                                        return Ok(Request::Echo(s));
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        }
-                        _ => {}
+            RespValue::Array(elements) => match elements.get(0) {
+                Some(RespValue::BulkString(s)) => match s.to_lowercase().as_str() {
+                    "ping" => Ok(Request::Ping),
+                    "echo" => match elements.get(1) {
+                        Some(RespValue::BulkString(s)) => Ok(Request::Echo(s)),
+                        _ => Err(anyhow::format_err!("malformed ECHO command")),
                     },
-                    _ => {}
-                }
-            }
-            _ => {}
+                    "command" => match elements.get(1) {
+                        Some(RespValue::BulkString(s)) => match s.to_lowercase().as_str() {
+                            "docs" => Ok(Request::CommandDocs),
+                            _ => Err(anyhow::format_err!("malformed COMMAND DOCS command")),
+                        },
+                        _ => Err(anyhow::format_err!("malformed COMMAND DOCS command")),
+                    },
+                    "set" => {
+                        let key = match elements.get(1) {
+                            Some(RespValue::BulkString(s)) => *s,
+                            _ => return Err(anyhow::format_err!("malformed SET command")),
+                        };
+                        let value = match elements.get(2) {
+                            Some(RespValue::BulkString(s)) => *s,
+                            _ => return Err(anyhow::format_err!("malformed SET command")),
+                        };
+                        Ok(Request::Set { key, value })
+                    }
+                    "get" => {
+                        let key = match elements.get(1) {
+                            Some(RespValue::BulkString(s)) => *s,
+                            _ => return Err(anyhow::format_err!("malformed GET command")),
+                        };
+                        Ok(Request::Get(key))
+                    }
+                    command => Err(anyhow::format_err!(
+                        "unhandled command {:?}",
+                        command.to_uppercase()
+                    )),
+                },
+                _ => Err(anyhow::format_err!(
+                    "requests must start with a bulk string"
+                )),
+            },
+            _ => Err(anyhow::format_err!("requests must be arrays")),
         }
-        Err(anyhow::format_err!("unhandled request"))
     }
+}
 
-    fn generate_response(&self) -> anyhow::Result<Response> {
-        match self {
+#[derive(Default)]
+struct Memory {
+    data: HashMap<String, String>,
+}
+
+impl Memory {
+    fn handle_request<'request, 'memory>(
+        &'memory mut self,
+        request: &'request Request,
+    ) -> anyhow::Result<Response<'request, 'memory>> {
+        match request {
             Request::Ping => Ok(Response::Pong),
             Request::Echo(message) => Ok(Response::Echo(message)),
+            Request::CommandDocs => Ok(Response::CommandDocs),
+            Request::Set { key, value } => {
+                self.data.insert(key.to_string(), value.to_string());
+                Ok(Response::Set(SetResponse::Ok))
+            }
+            Request::Get(key) => match self.data.get(*key) {
+                Some(value) => Ok(Response::Get(GetResponse::Found(value))),
+                None => Ok(Response::Get(GetResponse::NotFound)),
+            },
         }
     }
 }
 
 #[derive(Debug)]
-enum Response<'data> {
+enum Response<'request, 'memory> {
+    CommandDocs,
     Pong,
-    Echo(&'data str),
+    Echo(&'request str),
+    Set(SetResponse),
+    Get(GetResponse<'memory>),
 }
 
-impl<'data> Response<'data> {
+#[derive(Debug)]
+enum GetResponse<'memory> {
+    Found(&'memory str),
+    NotFound,
+}
+
+#[derive(Debug)]
+enum SetResponse {
+    Ok,
+}
+
+impl<'request, 'memory> Response<'request, 'memory> {
     fn serialize(&self) -> Vec<u8> {
         match self {
-            Response::Pong => RespValue::SimpleString("PONG").serialize(),
-            Response::Echo(s) => RespValue::BulkString(s).serialize(),
+            Response::Pong => RespValue::SimpleString("PONG"),
+            Response::Echo(s) => RespValue::BulkString(s),
+            Response::CommandDocs => RespValue::Array(vec![]),
+            Response::Set(set_response) => match set_response {
+                SetResponse::Ok => RespValue::SimpleString("OK"),
+            },
+            Response::Get(get_response) => match get_response {
+                GetResponse::Found(value) => RespValue::BulkString(value),
+                GetResponse::NotFound => RespValue::Null,
+            },
         }
+        .serialize()
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream, memory: Arc<Mutex<Memory>>) {
     let mut buf = [0; 512];
     loop {
         match stream.read(&mut buf).await {
@@ -85,19 +150,20 @@ async fn handle_connection(mut stream: TcpStream) {
                     continue;
                 }
 
-                if let Ok(request) = Request::deserialize(&buf[0..bytes_read]) {
-                    if let Ok(response) = request.generate_response() {
-                        let resp = response.serialize();
-                        eprintln!("Response: {:?}", std::str::from_utf8(&resp).unwrap());
+                match Request::deserialize(&buf[0..bytes_read]) {
+                    Ok(request) => {
+                        let response = memory
+                            .lock()
+                            .expect("failed to get lock")
+                            .handle_request(&request)
+                            .unwrap()
+                            .serialize();
                         stream
-                            .write_all(&resp)
+                            .write_all(&response)
                             .await
                             .expect("failed to write to stream");
-                    } else {
-                        eprintln!("failed to generate a response");
                     }
-                } else {
-                    eprintln!("failed to parse request");
+                    Err(e) => eprintln!("failed to deserialize request: {:?}", e),
                 }
             }
             Err(e) => {
@@ -110,12 +176,14 @@ async fn handle_connection(mut stream: TcpStream) {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind(SocketAddrV4::new(ADDRESS, PORT)).await?;
+    let memory = Arc::new(Mutex::new(Memory::default()));
 
+    let listener = TcpListener::bind(SocketAddrV4::new(ADDRESS, PORT)).await?;
     loop {
         let (stream, _) = listener.accept().await?;
+        let thread_memory = memory.clone();
         tokio::spawn(async move {
-            handle_connection(stream).await;
+            handle_connection(stream, thread_memory).await;
         });
     }
 }
